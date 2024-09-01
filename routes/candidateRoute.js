f0da89132491c1bbc2fa076361f162f6a3d3b110
Gul <gulhassanabd@gmail.com>
2024-09-01 06:19:37 -0700

    little change in routes

--- routes/candidateRoute.js
+++ routes/candidateRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const jwtAuth = require('../jwtAuth');
 const candidateController = require('../controller/candidateController');
 
-router.post('/', jwtAuth, candidateController.register); // only Admin can lists the Candidates for election
+router.post('/candidates', jwtAuth, candidateController.register); // only Admin can lists the Candidates for election
 router.put('/:candidateID', jwtAuth, candidateController.updateCandidate); //only Admin can update data of Candidates
 router.delete('/:candidateID', jwtAuth, candidateController.removeCandidate) // only Admin can delete data of Candidates
 router.post('/vote/:candidateID', jwtAuth, candidateController.userVote); // only user can vote 
