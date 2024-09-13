f3251f76bb1cbab6b200ded7cb6d071c70f6cd9b
Gul <gulhassanabd@gmail.com>
2024-09-13 10:23:48 -0700

    added fronttend link

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const corsOptions = {
   origin: (origin, callback) => {
     const allowedOrigins = [
       'https://votingappmadysteam.netlify.app',
+      'https://voting-app-frontend-one.vercel.app',
       'http://localhost:3000'
     ];
     if (!origin || allowedOrigins.includes(origin)) {
