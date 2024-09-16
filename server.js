4a62c0b7a32763a1aef7d132ddf1dd6c9715a7b1
Gul <gulhassanabd@gmail.com>
2024-09-16 03:42:07 -0700

    added options in cors

--- server.js
+++ server.js
@@ -1,57 +1,42 @@
-const cluster = require('cluster');
-const http = require('http');
-const os = require('os');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
+const app = express();
 require('dotenv').config();
 require('./database/connection');
 
 // Define CORS options
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://voting-app-vite-frontend-afw2.vercel.app'], // Allow requests from these origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  origin: ['https://voting-app-vite-frontend-afw2.vercel.app', 'http://localhost:5173'], // Allow requests from these origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods including OPTIONS for preflight
   allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'], // Specify allowed headers
   credentials: true // Allow credentials such as cookies and HTTP authentication
 };
 
-if (cluster.isMaster) {
-  const numCPUs = os.cpus().length;
-  console.log(`Master ${process.pid} is running`);
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// Handle preflight requests
+app.options('*', cors(corsOptions)); // Preflight requests for all routes
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  // Apply CORS middleware
-  app.use(cors(corsOptions));
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to Voting App");
+});
 
-  // Serve static files from the 'uploads' directory
-  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const userRoute = require('./routes/userRoute');
+app.use('/user', userRoute);
 
-  app.get("/", (req, res) => {
-    res.send("Welcome to Voting App");
-  });
+const candidateRoute = require('./routes/candidateRoute');
+app.use('/candidate', candidateRoute);
 
-  const userRoute = require('./routes/userRoute');
-  app.use('/user', userRoute);
-
-  const candidateRoute = require('./routes/candidateRoute');
-  app.use('/candidate', candidateRoute);
-
-  const PORT = process.env.PORT || 4000;
-  http.createServer(app).listen(PORT, () => {
-    console.log(`Worker ${process.pid} running on port# ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Your server is running on port# ${PORT}`);
+});
