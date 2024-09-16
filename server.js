477d15dacbfd6ceb9517ec153c99872f57d21a9d
Gul <gulhassanabd@gmail.com>
2024-09-16 03:52:54 -0700

    removed cluster

--- server.js
+++ server.js
@@ -1,14 +1,50 @@
-const cluster = require('cluster');
-const http = require('http');
-const os = require('os');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
+const app = express();
 require('dotenv').config();
 require('./database/connection');
 
+
 // Define CORS options
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     const allowedOrigins = [
+//       'https://voting-app-vite-frontend.vercel.app',
+//       'http://localhost:3000',
+//       'http://localhost:5173'
+//     ];
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true); // Allow the request
+//     } else {
+//       callback(new Error('Not allowed by CORS')); // Reject the request
+//     }
+//   },
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+//   allowedHeaders: 'Content-Type,Authorization,x-access-token',
+//   credentials: true // Allow credentials
+// };
+
+// // Apply CORS middleware
+// app.use(cors(corsOptions));
+
+// // For preflight requests
+// app.options('*', cors(corsOptions));
+
+// const allowedOrigins = ['http://localhost:5173'];
+
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
+
+// Configure CORS
 const corsOptions = {
   origin: ['http://localhost:5173', 'https://voting-app-vite-frontend-afw2.vercel.app'], // Allow requests from these origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
@@ -16,42 +52,27 @@ const corsOptions = {
   credentials: true // Allow credentials such as cookies and HTTP authentication
 };
 
-if (cluster.isMaster) {
-  const numCPUs = os.cpus().length;
-  console.log(`Master ${process.pid} is running`);
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  // Apply CORS middleware
-  app.use(cors(corsOptions));
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    res.send("welcome to voting app");
+});
 
-  // Serve static files from the 'uploads' directory
-  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const userRoute = require('./routes/userRoute');
+app.use('/user', userRoute);
 
-  app.get("/", (req, res) => {
-    res.send("Welcome to Voting App");
-  });
+const candidateRoute = require('./routes/candidateRoute');
+app.use('/candidate', candidateRoute);
 
-  const userRoute = require('./routes/userRoute');
-  app.use('/user', userRoute);
-
-  const candidateRoute = require('./routes/candidateRoute');
-  app.use('/candidate', candidateRoute);
-
-  const PORT = process.env.PORT || 4000;
-  http.createServer(app).listen(PORT, () => {
-    console.log(`Worker ${process.pid} running on port# ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Your server is running on port# ${PORT}`);
+});
