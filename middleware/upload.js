15faf3139b05834a564ab4adc4b788befe378d79
Gul <gulhassanabd@gmail.com>
2024-09-17 03:55:25 -0700

    changes in uploads

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,10 +1,11 @@
 const multer = require('multer');
 const path = require('path');
 
+
 // Multer setup for image upload
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'upload/'); // Directory to save uploaded files
+        cb(null, '../uploads'); // Directory to save uploaded files
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to file name
