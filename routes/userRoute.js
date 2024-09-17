8146fc8c860ec555f7ec20c09356c5eb9856b3bd
Gul <gulhassanabd@gmail.com>
2024-09-17 01:34:05 -0700

    changed uploads

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controller/userController');
 const jwtAuth = require('../jwtAuth');
-const upload = require('../middleware/upload'); // Import your multer configuration
+const uploadMiddleware = require('../middleware/upload'); // Import your multer configuration
 
 
-router.post('/signup', upload, userController.register);; //register in VotingApp
+router.post('/signup', uploadMiddleware , userController.register);; //register in VotingApp
 router.post('/login', userController.authenticate); //Login via identityCard and Password
 router.put('/:userId', userController.updateUser); // User can update his password using identitycardNumber and currentPassword
 router.get('/userProfiles', jwtAuth, userController.getAll); // only admin can access he can see the data of users
